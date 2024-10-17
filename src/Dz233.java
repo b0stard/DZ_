c412aab0265971cd7de357dd349e16f36de75bec
b0stard <sostanin378@gmail.com>
2024-10-17 19:32:43 +0300

    7.1

---
+++ src/Dz233.java
@@ -0,0 +1,10 @@
+public class Dz233 {
+    public static void main(String[] args) {
+        System.out.println("Целые числа от 100 до 200, кратные трем:");
+        for (int i = 100;i <=200;i++){
+            if(i % 3==0){
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
