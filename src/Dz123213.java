88694748cba117bf9e7dd019d0c91fd8661c26d5
b0stard <sostanin378@gmail.com>
2024-10-18 21:39:27 +0300

    8.1

---
+++ src/Dz123213.java
@@ -0,0 +1,15 @@
+public class Dz123213 {
+    public static void main(String[] args) {
+        int row = 4;
+        int bounty = 6;
+        int number = 5;
+
+        for (int i = 0;i < row;i++){
+            for (int j = 0;j <bounty;j++){
+                System.out.print(number + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
